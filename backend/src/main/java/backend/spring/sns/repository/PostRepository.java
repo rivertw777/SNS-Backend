4126c4e4c5365b18f76637a3a06ef7f8b8d037f0
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-01-03 20:27:59 +0900

    test: Jenkins 빌드 테스트

--- backend/src/main/java/backend/spring/sns/repository/PostRepository.java
+++ backend/src/main/java/backend/spring/sns/repository/PostRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
     Optional<Post> findByPostId(Long postId);
+
 }
