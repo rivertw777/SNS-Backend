a2d1fda27448cb258e435fe04902e6de84221afc
rivertw777 <105557972+rivertw777@users.noreply.github.com>
2024-01-19 03:45:13 +0900

    refactor: 사진 경로 환경변수 이동

--- backend/src/main/java/backend/spring/file/FileSystemService.java
+++ backend/src/main/java/backend/spring/file/FileSystemService.java
@@ -1,25 +1,22 @@
 package backend.spring.file;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.io.File;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.core.env.Environment;
 
 @Profile("local")
 @RequiredArgsConstructor
 @Service
 public class FileSystemService {
 
-    @Autowired
-    private final Environment environment;
-
     public String uploadPhoto(MultipartFile photo, String photoName) throws IOException {
-        String uploadPath = environment.getProperty("photo.save.dir");
-        String accessUrl = environment.getProperty("photo.access.url");
+        Dotenv dotenv = Dotenv.load();
+        String uploadPath = dotenv.get("photo.save.dir");
+        String accessUrl = dotenv.get("photo.access.url");
 
         // 사진 파일 저장
         String projectPath = System.getProperty("user.dir") + uploadPath;
--- backend/src/main/java/backend/spring/sns/service/impl/PhotoUploader.java
+++ backend/src/main/java/backend/spring/sns/service/impl/PhotoUploader.java
@@ -4,22 +4,15 @@ import backend.spring.file.FileSystemService;
 import backend.spring.file.S3Service;
 import com.amazonaws.services.s3.AmazonS3Client;
 import java.io.IOException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-@RequiredArgsConstructor
 @Component
 public class PhotoUploader {
 
-    @Autowired
-    private final Environment environment;
-
     public String uploadPhoto(MultipartFile photo, String photoName) throws IOException {
         if ("local".equals(System.getProperty("spring.profiles.active"))) {
-            FileSystemService fileSystemService = new FileSystemService(environment);
+            FileSystemService fileSystemService = new FileSystemService();
             return fileSystemService.uploadPhoto(photo, photoName);
         }
         S3Service s3Service = new S3Service(new AmazonS3Client());
